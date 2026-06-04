c85fc279e77744ef0e6fe08e6a1c0d0304a7fcc7
volunteer1985 <80355284+volunteer1985@users.noreply.github.com>
2026-06-04 22:29:02 +0300

    turtle draws multiple polygons up to given number of angles

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import random
 import turtle
 from turtle import Turtle, Screen
+def get_random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    return r, g, b
 
 def draw_a_line(distance):
     tortilla.pendown()
@@ -27,13 +33,25 @@ def draw_a_dashed_line(dash_length, line_length):
         draw_a_space(dash_length)
         line_length -= dash_length
 
+def draw_a_polygon(corners, length):
+    for i in range(corners):
+        move_and_turn_left(length, 360 / corners)
+
+def draw_multiple_polygons(up_to_angles, side):
+    start_angles = 3
+    while up_to_angles >= start_angles:
+        draw_a_polygon(start_angles, side)
+        start_angles += 1
+
+
+
 
 tortilla = Turtle()
 tortilla.shape("turtle")
 tortilla.color("deep pink")
 
-draw_a_dashed_line(10, 100)
 
+draw_multiple_polygons(50, 10)
 
 
 
